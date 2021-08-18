5647c93bb71b7dc7f3e07e201d66f6fb72c9bdd3
Adrian Warman <adrian.warman@digital.justice.gov.uk>
2021-08-18 12:24:55 +0100

    Complete draft of RSS and Changelog capability.

--- buildUtils/generateRSS.py
+++ buildUtils/generateRSS.py
@@ -1,19 +1,22 @@
 from feedgen.feed import FeedGenerator
 import csv
 
-outFileDita = open('../dita/changelog.dita','w')
+siteURL = "https://security-guidance.service.justice.gov.uk/"
+
+outFileDita = open('changelog.dita','w')
 outFileDita.write('<?xml version="1.0" encoding="UTF-8"?>\n')
 outFileDita.write('<!DOCTYPE topic PUBLIC "-//OASIS//DTD DITA Topic//EN" "topic.dtd">\n')
 outFileDita.write('<topic id="changelog">\n')
-outFileDita.write('<title>Changelog for <ph conref="conrefs.dita#conrefs/mojlong"/> Security Guidance</title>\n')
+outFileDita.write('<title>Change log for <ph conref="conrefs.dita#conrefs/moj"/> Security Guidance</title>\n')
 outFileDita.write('<body>\n')
-outFileDita.write('<p>This document summarises what changes were made, and when, to <ph conref="conrefs.dita#conrefs/moj"/> Security policy and guidance. The most recent changes appear at the end of the list.</p>\n')
+outFileDita.write('<p>This document summarises what changes were made, and when, to <ph conref="conrefs.dita#conrefs/moj"/> Security policy and guidance. The most recent changes appear at the beginning of the list.</p>\n')
+outFileDita.write('<dl>\n')
 
 fg = FeedGenerator()
 fg.id('https://security-guidance.service.justice.gov.uk')
 fg.title('MoJ Security Guidance')
 fg.author( {'name':'Ministry of Justice','email':'itsecuritypolicy@digital.justice.gov.uk'} )
-fg.link( href='https://security-guidance.service.justice.gov.uk', rel='alternate' )
+# fg.link( href='https://security-guidance.service.justice.gov.uk', rel='alternate' )
 fg.logo('https://security-guidance.service.justice.gov.uk/favicon.ico')
 fg.subtitle('This site documents the Ministry of Justice (MoJ) security policies and guidance.')
 fg.link( href='https://security-guidance.service.justice.gov.uk', rel='self' )
@@ -24,21 +27,30 @@ with open('../changeLog.csv') as csvDataFile:
     csvReader = csv.reader(csvDataFile)
     loopCounter = 0
     entryList = []
+    ditaEntries = []
     for row in csvReader:
         entryList.append(fg.add_entry())
-        entryList[loopCounter].id("'"+row[0]+"'")
-        entryList[loopCounter].title("'"+row[1]+"'")
-        entryList[loopCounter].description("'"+row[2]+"'")
-        entryList[loopCounter].link( href="'"+row[3]+"'")
-        entryList[loopCounter].pubDate("'"+row[4]+"'")
+        entryList[loopCounter].id(""+siteURL+row[0]+"")
+        entryList[loopCounter].title(""+row[1]+"")
+        entryList[loopCounter].description(""+row[2]+"")
+        entryList[loopCounter].link( href=""+siteURL+row[3]+"")
+        entryList[loopCounter].pubDate(""+row[4]+"")
+        ditaEntries.insert(0,"<dlentry><dt>"+row[4]+" <xref href='"+siteURL+row[3]+"' format='html' scope='external'>"+row[1]+"</xref></dt><dd>"+row[2]+"</dd></dlentry>")
         loopCounter = loopCounter + 1
 
-print('Finished looping.')
 
 atomfeed = fg.atom_str(pretty=True)
 rssfeed  = fg.rss_str(pretty=True)
 fg.atom_file('atom.xml')
 fg.rss_file('rss.xml')
+
+for row in ditaEntries:
+    outFileDita.write(row+"\n")
+outFileDita.write('</dl>\n')
+outFileDita.write('</body>\n')
+outFileDita.write('<topic id="contact-details" conref="conrefs.dita#contact-details"><title/></topic>\n')
+outFileDita.write('<topic id="feedback" platform="html" conref="conrefs.dita#feedback"><title/></topic>\n')
+outFileDita.write('</topic>\n')
 
 outFileDita.close
 
---
+++ docs/changelog.md
@@ -0,0 +1,25 @@
+# Change log for MoJ Security Guidance
+
+This document summarises what changes were made, and when, to MoJ Security policy and guidance. The most recent changes appear at the beginning of the list.
+
+-   **2021-08-17 09:26:00 BST [Provide offline version of Group Security content.](https://security-guidance.service.justice.gov.uk/#offline-content)**
+
+    Created PDF and eBook versions of the security policy and guidance subset for Group Security.
+
+-   **2021-08-16 17:04:00 BST [Clarification for accessing MoJ IT systems overseas.](https://security-guidance.service.justice.gov.uk/accessing-moj-it-systems-from-overseas/)**
+
+    Additional information describing the process.
+
+-   **2021-08-16 17:03:00 BST [Data Movement Form updated.](https://security-guidance.service.justice.gov.uk/gs/data-movement-form.docx)**
+
+    Data Movement Form updated.
+
+
+## Contact details
+
+For any further questions relating to security, contact: [security@justice.gov.uk](mailto:security@justice.gov.uk), or for security advice, contact the Cyber Assistance Team [CyberConsultancy@digital.justice.gov.uk](mailto:CyberConsultancy@digital.justice.gov.uk).
+
+## Feedback
+
+> If you have any questions or comments about this guidance, such as suggestions for improvements, please contact: [itpolicycontent@digital.justice.gov.uk](mailto:itpolicycontent@digital.justice.gov.uk).
+
