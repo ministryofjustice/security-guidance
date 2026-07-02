2fcdb295beb00a481a86c5d37574ad55c077bcbe
MoJEProsser <edward.prosser@justice.gov.uk>
2026-07-02 13:22:15 +0100

    remove other pages correct redirect

--- docs/iasme/Policy_Templates/createList.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-echo "# IASME templates" > index.md
-echo "" >> index.md
-for i in *
-do
-	label=`sed 's/_TEMPLATE_V1//; s/_GUIDELINES_V1//; s/_/ /g; s/.docx//; s/.odt//; s/.ods//; s/.xlsx//' <(echo "$i")`
-	echo "[$label TEMPLATE]($i)" >> index.md
-done
